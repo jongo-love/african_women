b6f049a6f2d2d1d3410e8f5517ad333555154a40
jongo-love <jongolove01@gmail.com>
2024-11-08 11:29:36 +0300

    login and registration working

--- store/ecommerce/urls.py
+++ store/ecommerce/urls.py
@@ -1,6 +1,7 @@
 # ecommerce/urls.py
 
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'ecommerce'
@@ -9,4 +10,7 @@ urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('category/<slug:category_slug>/', views.product_list, name='product_list_by_category'),
     path('product/<int:product_id>/', views.product_detail, name='product_detail'),
+    path('login/', auth_views.LoginView.as_view(template_name='ecommerce/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='ecommerce/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
 ]
--- store/ecommerce/views.py
+++ store/ecommerce/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Product, Category
+from .forms import UserRegistrationForm
 # Create your views here.
 def product_list(request, category_slug=None):
     category = None
@@ -22,3 +23,31 @@ def product_detail(request, product_id):
     product = get_object_or_404(Product, id=product_id, available=True)
     context = {'product': product}
     return render(request, 'ecommerce/product_detail.html', context)
+
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('ecommerce:product_list')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'ecommerce/register.html', {'form': form})
+
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
+
+@login_required
+def admin_dashboard(request):
+    if request.user.is_staff:
+        # Redirect to Django's default admin panel
+        return redirect('/admin/')
+    else:
+        return HttpResponseForbidden("You do not have access to this page.")
